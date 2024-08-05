451c11ac3c3dda8d697261361ca0a0d80714acb2
Isaque Lis <isaquelis.g@gmail>
2024-08-05 03:31:41 -0300

    new: note-component

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-const app = document.getElementById("App");
+import SimpleNote from "./simple-note-component/SimpleNote.js";
+
+customElements.define("simple-note", SimpleNote);
+
+
+document.getElementById("make-a-note").addEventListener("click", function () {
+    const note = new SimpleNote();
+    document.getElementById("notes-placer").appendChild(note);
+})
---
+++ js/simple-note-component/SimpleNote.js
@@ -0,0 +1,113 @@
+const template = document.createElement("template");
+
+template.innerHTML = `
+<link rel="stylesheet" href="./css/reset.css">
+<link rel="stylesheet" href="./js/simple-note-component/note-component-styles.css">
+<link rel="stylesheet" href="./css/notes-colors.css">
+<section id="simple-note" class="note">
+    <input
+        class="note-title"
+        type="text"
+        name="title"
+        placeholder="Título">
+
+    <textarea class="note-content" name="content" placeholder="Escreva sua nota aqui..."></textarea>
+
+</section>
+`
+
+class SimpleNote extends HTMLElement {
+
+    #id;
+    #name;
+    #color;
+    
+    #text;
+    #createdDate;
+    #modifiedDate;
+
+    #state;
+
+    /**
+     * @param {string} name 
+     * @param {string} color 
+     */
+    constructor (title, color) {
+        
+        super();
+
+        this.attachShadow({mode: "open"});
+
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        this.title = title;
+        this.color = color;
+
+    }
+
+    static get observedAttributes() {
+        return ["title", "color"];
+    }
+
+    connectedCallback() {
+        
+    }
+
+    attributeChangedCallback (name, oldValue, newValue) {
+
+        if(name === "title") {
+            this.title = newValue;
+        }
+
+        if(name === "color") {
+
+            this.color = newValue; 
+        }
+    }
+
+    disconnectedCallback() {
+
+    }
+
+    set id(newValue) {
+        this.#id = newValue;
+    }
+
+    set title(newValue) {
+        this.#name = newValue;
+    }
+
+    set color(newValue) {
+        const validColors = ["pink", "blue", "yellow", "green"];
+        
+        if(!newValue || newValue == "" || validColors.includes(newValue)){
+            newValue = validColors[Math.floor(Math.random() * validColors.length)];
+        }
+
+        console.log(this.shadowRoot.firstElementChild.nextElementSibling);
+        const classList = this.shadowRoot.getElementById("simple-note").classList;
+
+        if(classList.length > 1) {
+            classList.remove(classList[classList.length - 1]);
+            
+        } 
+                
+        classList.add("notes-bg-" + newValue);
+        
+    }
+
+    get id() {
+        this.#id;
+    }
+
+    get title() {
+        this.#name;
+    }
+
+    get color() {
+        this.#color;
+    }
+    
+}
+
+export default SimpleNote;
