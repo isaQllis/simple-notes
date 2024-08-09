571d9b2ed08bb302a007e6cf06c472f999cb9cda
Isaque Lis <104284825+isaQllis@users.noreply.github.com>
2024-08-08 21:03:33 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import SimpleNote from "isaqllis.github.io/simple-notes/src/components/simple-note/SimpleNote.js";
-import SimpleButton from "isaqllis.github.io/simple-notes/src/components/simple-button/SimpleButton.js";
+import SimpleNote from "/src/components/simple-note/SimpleNote.js";
+import SimpleButton from "/src/components/simple-button/SimpleButton.js";
 
-import makeNote from "isaqllis.github.io/simple-notes/src/scripts/listeners/makeNote.js";
-import turnLightMode from "isaqllis.github.io/simple-notes/src/scripts/listeners/turnLightMode.js";
+import makeNote from "/src/scripts/listeners/makeNote.js";
+import turnLightMode from "/src/scripts/listeners/turnLightMode.js";
 
 customElements.define("simple-note", SimpleNote);
 customElements.define("simple-button", SimpleButton);
